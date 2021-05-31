2b9d21de2ffadf0fd3e150c25c4cf6797e4c827a
Kanda, Jiří <kanda@havit.cz>
2021-05-31 14:31:45 +0200

    Initial commit

---
+++ WebApplication1/Pages/CustomValidation.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Pages
+{
+    public class CustomValidation : ComponentBase
+    {
+        private ValidationMessageStore messageStore;
+
+        [CascadingParameter]
+		private EditContext CurrentEditContext { get; set; }
+
+        protected override void OnInitialized()
+        {
+            if (CurrentEditContext == null)
+            {
+                throw new InvalidOperationException(
+                    $"{nameof(CustomValidation)} requires a cascading " +
+                    $"parameter of type {nameof(EditContext)}. " +
+                    $"For example, you can use {nameof(CustomValidation)} " +
+                    $"inside an {nameof(EditForm)}.");
+            }
+
+            messageStore = new(CurrentEditContext);
+
+            CurrentEditContext.OnValidationRequested += (s, e) => messageStore.Clear();
+            CurrentEditContext.OnFieldChanged += (s, e) => messageStore.Clear(e.FieldIdentifier);
+        }
+
+        public void ClearErrors()
+        {
+            messageStore.Clear();
+            CurrentEditContext.NotifyValidationStateChanged();
+        }
+
+        public void AddError(string fieldName, string error)
+        {
+            messageStore.Add(CurrentEditContext.Field(fieldName), error);
+            CurrentEditContext.NotifyValidationStateChanged();
+        }
+
+    }
+}
---
+++ WebApplication1/Pages/ExampleModelValidation.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Pages
+{
+	public class ExampleModelValidation : ComponentBase
+	{
+        private ValidationMessageStore messageStore;
+
+        [CascadingParameter]
+        private EditContext CurrentEditContext { get; set; }
+
+        protected override void OnInitialized()
+        {
+            if ((CurrentEditContext == null) || (CurrentEditContext.Model == null) || (CurrentEditContext.Model is not Demo09.ExampleModel))
+			{
+                throw new InvalidOperationException();
+            }
+            
+            messageStore = new ValidationMessageStore(CurrentEditContext);
+
+            CurrentEditContext.OnValidationRequested += (sender, eventArgs) => Validate();
+            CurrentEditContext.OnFieldChanged += (sender, eventArgs) => messageStore.Clear(eventArgs.FieldIdentifier);
+        }
+
+        public void Validate()
+        {
+            messageStore.Clear();
+
+            Demo09.ExampleModel model = (Demo09.ExampleModel)CurrentEditContext.Model;
+
+            if (String.IsNullOrEmpty(model.Name))
+			{
+                messageStore.Add(CurrentEditContext.Field(nameof(model.Name)), "Please fill the name (from custom validator).");
+            }
+
+            CurrentEditContext.NotifyValidationStateChanged();
+        }
+    }
+}
